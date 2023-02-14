e77693de5347343b392c9c01369270ff1c732577
jbamlee <joon@delightlabs.io>
2023-02-14 12:53:18 +0900

    feat: initial token info of XPLA

--- db/migration/indexer/20230125_175830_create_token.go
+++ db/migration/indexer/20230125_175830_create_token.go
@@ -9,10 +9,43 @@ import (
 	"gorm.io/gorm"
 )
 
+var (
+	MainnetToken = indexer.Token{
+		ChainModel: indexer.ChainModel{
+			ChainId: "dimension_37-1",
+			Address: "axpla",
+		},
+		Protocol: "",
+		Symbol:   "XPLA",
+		Name:     "XPLA",
+		Decimals: 18,
+		Icon:     "https://assets.xpla.io/icon/svg/XPLA.svg",
+		Verified: true,
+	}
+	TestnetToken = indexer.Token{
+		ChainModel: indexer.ChainModel{
+			ChainId: "cube_47-5",
+			Address: "axpla",
+		},
+		Protocol: "",
+		Symbol:   "XPLA",
+		Name:     "XPLA",
+		Decimals: 18,
+		Icon:     "https://assets.xpla.io/icon/svg/XPLA.svg",
+		Verified: true,
+	}
+)
+
 var M20230125_175830 = &gormigrate.Migration{
 	ID: "20230125_175830",
 	Migrate: func(tx *gorm.DB) error {
-		return tx.AutoMigrate(&indexer.Token{})
+		if err := tx.AutoMigrate(&indexer.Token{}); err != nil {
+			return err
+		}
+		if err := tx.Save([]indexer.Token{MainnetToken, TestnetToken}).Error; err != nil {
+			return err
+		}
+		return nil
 	},
 	Rollback: func(tx *gorm.DB) error {
 		return tx.Migrator().DropTable(&indexer.Token{})
